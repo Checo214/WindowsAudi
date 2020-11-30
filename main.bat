6aa0db297c885f4cf949fdb8ffcb573ef441ac2e
Checo214 <up190136@alumnos.upa.edu.mx>
2020-11-30 08:18:08 -0600

    Cambio en las primeras dos lineas

--- main.bat
+++ main.bat
@@ -1,10 +1,2 @@
- El volumen de la unidad C es Windows
- El n£mero de serie del volumen es: AA30-FC30
-
- Directorio de C:\Users\nao_c\OneDrive\Documents\Uni_Checo\3er cuatri\Mantenimiento\winAudit
-
-24/11/2020  08:02 a. m.    <DIR>          .
-24/11/2020  08:02 a. m.    <DIR>          ..
-24/11/2020  08:10 a. m.                 0 main.bat
-               1 archivos              0 bytes
-               2 dirs  24,916,246,528 bytes libres
+@echo off
+echo Hola soy un cambio
